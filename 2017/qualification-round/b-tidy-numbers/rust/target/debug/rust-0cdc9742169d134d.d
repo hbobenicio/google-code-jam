dd99ec477afa716b18ec3d2edd8e35974f155db2
Hugo Benicio <hbobenicio@gmail.com>
2018-02-16 18:43:17 -0300

    Removendo diretório target

--- 2017/qualification-round/b-tidy-numbers/rust/target/debug/rust-0cdc9742169d134d.d
+++
@@ -1,1 +0,0 @@
-/home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/target/debug/rust-0cdc9742169d134d: /home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/main.rs
--- 2017/qualification-round/b-tidy-numbers/rust/target/rls/debug/deps/rust-27506eefdd9bf65c.d
+++
@@ -1,7 +0,0 @@
-/home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/target/rls/debug/deps/rust-27506eefdd9bf65c.rmeta: /home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/main.rs /home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/gcj.rs /home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/scanner/mod.rs
-
-/home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/target/rls/debug/deps/rust-27506eefdd9bf65c.d: /home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/main.rs /home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/gcj.rs /home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/scanner/mod.rs
-
-/home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/main.rs:
-/home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/gcj.rs:
-/home/01372329340/projetos/google-code-jam/2017/qualification-round/b-tidy-numbers/rust/src/scanner/mod.rs:
