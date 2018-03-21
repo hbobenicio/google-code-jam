7ee78edf52a783743046817298c5a503b2eace09
Hugo Benicio <hbobenicio@gmail.com>
2018-03-21 14:31:10 -0300

    Creating a helper library (TODO: move this to independent repo)

---
+++ libs/gcj-rs/src/gcj/io.rs
@@ -0,0 +1,49 @@
+use std::io;
+use std::io::prelude::BufRead;
+
+pub struct Scanner {
+    stdin: io::Stdin,
+    capacity: usize
+}
+
+impl Scanner {
+    pub fn new() -> Scanner {
+        Scanner {
+            stdin: io::stdin(),
+            capacity: 0
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Scanner {
+        Scanner {
+            stdin: io::stdin(),
+            capacity
+        }
+    }
+
+    pub fn read_line(&self) -> String {
+        let mut line = String::with_capacity(self.capacity);
+
+        self.stdin
+            .read_line(&mut line)
+            .expect("Error while reading line from stdin");
+
+        line
+    }
+
+    /// Reads all lines from stdin.
+    /// This ignores empty lines.
+    pub fn all_lines<F>(&self, f: F)
+    where
+        F: Fn((usize, String))
+    {
+        for x in self.stdin.lock().lines()
+            .map(|l| l.expect("gcj::utils - Error while reading line from stdin"))
+            .filter(|l| l.trim() != "")
+            .enumerate()
+            .map(|(i, l)| (i+1, l))
+        {
+            f(x);
+        };
+    }
+}
---
+++ libs/gcj-rs/src/lib.rs
@@ -0,0 +1,10 @@
+mod gcj;
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        let scanner = gcj::io::Scanner::new();
+    }
+}
